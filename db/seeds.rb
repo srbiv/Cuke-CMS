5448794751da43f33822b64ba3942dd6c4bca500
Stafford Brooke <staffordbrooke@gmail.com>
2011-04-17 15:48:50 -0400

    styling, content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Just some sample content to manage
+lol# Just some sample content to manage
 
 documentation = ContentType.create(:name => 'documentation')
 pages         = ContentType.create(:name => 'pages')
@@ -25,7 +25,7 @@ BODY3
   Content.create(:title => 'Pages vs Content',
                  :slug  => 'pages-vs-content',
                  :body  => <<-BODY4
-I have no idea what the difference between Content and Pages is. Edit this page and improve me!
+Content<br/> Think of content as a single instance of a category. Think of people as a content type. Each person is a content instance. Loren Norman is a content instance. A person content type would contain all the attributes that make Loren Norman a unique person. <br/> Outside of the human world there are other content types. They all have names to identify what is what. Outside of that, they contain the attributes they need to be the things they are :) Pages<br/> Think of pages as containers. They will contain at least one piece of content (otherwise they would be blank). But could possibly contain many pieces of content. To continue with the people metaphor. A page could be a car, a house, a room in a house, even underwear. Let's explore the house example: <br/> A house holds a family of people, maybe a dog and a cat and probably some groceries. We put these guys on a page view blocks. In this example one block would be family: a list of people in the family, another would be a picture of the cat, yet another would be a picture of the dog. Then maybe the groceries are another list of what is about to get eaten. 
 BODY4
   )
 ]
